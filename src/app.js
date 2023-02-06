ec88f6161b44423e6b24005b20d12f7b39ef7e4d
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2023-02-06 15:43:24 +0100

    created during class

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import './index.html';
+import './css/style.css';
+import SearchImage from './searchImageComponent/searchImageComponent';
+import ListBreeds from './listBreedsComponent/listBreedsComponent';
+
+new SearchImage();
+
+new ListBreeds();
